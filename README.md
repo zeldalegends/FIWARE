0fa4abe6a86216c24fa08ed0f7bff07224d6775b
zeldalegends <gamesup@libero.it>
2024-04-23 04:31:12 +0000

    Initial commit

--- README.md
+++ README.md
@@ -3,11 +3,19 @@ FIWARE stack with enhanced security and blockchain.
 
 ## Setup (for Nginx and Let's Encrypt)
 Set your domain and email in the following files:
-- `compose.yaml`
-- `nginx/nginx.conf`
+- **compose.yaml***
+- **nginx/nginx.conf**
+- **run.sh**
 
 ## Usage
-`./run.sh` or `docker compose up -d` to start the containers.
+Initialize containers with:
+- `$ ./run.sh` (first time)
+
+Run containers with:
+- `$ docker compose up -d` (or `$ ./run.sh`)
+
+Stop containers with:
+- `$ docker compose down` (or `$ ./stop.sh`)
 
 ### Note
 For a security issue on Web3 libraries, the blockchain container is empty.
--- run.sh
+++ run.sh
@@ -9,26 +9,26 @@ domain=enershare.zapto.org
 email=test@email.com
 ##########################
 
-#sudo chown -R 0:0 .
-sudo chown -R $(id -u):$(id -g) .
-sudo chown -R 1000:1000 node-red
-docker compose pull
+#sudo chown -R $(id -u):$(id -g) .
+#sudo chown -R 1000:1000 node-red
+#docker compose pull
+docker compose down --remove-orphans --volumes
 
 # docker run --rm -v $(pwd)/node-red/data:/data node:18 bash -c "cd /data ; npm install --unsafe-perm --no-update-notifier --no-fund --only=production"
-# docker run --rm -p 80:80 -v ./certbot/conf/:/etc/letsencrypt/ -v ./certbot/www/:/var/www/certbot/ certbot/certbot certonly \
-#   --standalone --preferred-challenges=http \
-#   --domain $domain --email $email --no-eff-email --agree-tos --rsa-key-size 4096 --keep-until-expiring --non-interactive --verbose
+docker run --rm -p 80:80 --name certbot-run-once -v ./certbot/conf/:/etc/letsencrypt/ -v ./certbot/www/:/var/www/certbot/ certbot/certbot certonly \
+  --standalone --preferred-challenges=http -d $domain -m $email --no-eff-email --agree-tos --rsa-key-size 4096 --keep-until-expiring -n -v
+# docker run -d --rm -p 80:80 --name httpd-run-once -v ./certbot/www/:/usr/local/apache2/htdocs/ httpd:alpine
+# docker run --rm --name certbot-run-once -v ./certbot/conf/:/etc/letsencrypt/ -v ./certbot/www/:/var/www/certbot/ certbot/certbot certonly \
+#   --webroot -w /var/www/certbot/ -d $domain -m $email --no-eff-email --agree-tos --rsa-key-size 4096 --keep-until-expiring --non-interactive --verbose
+# docker stop httpd-run-once
 
-docker compose down --remove-orphans --volumes
 docker compose up --build --detach
 
-docker exec node-red bash -c "cd /data; npm install --unsafe-perm --no-update-notifier --no-fund --only=production"
-docker restart node-red
+#docker exec node-red bash -c "cd /data; npm install --unsafe-perm --no-update-notifier --no-fund --only=production"
+#docker restart node-red
 
 docker ps --all --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.ID}}" | sort
 read -s -n 1 -p "Press any key to continue (ESC to terminate)..." key && [[ $key != '' ]] && [[ $key == $'\e' ]] && echo && exit
-echo
-docker ps --all --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}" | sort
+echo && docker ps --all --format "table {{.Networks}}\t{{.Names}}\t{{.RunningFor}}\t{{.Image}}\t{{.Command}}\t{{.Mounts}}" | sort
 read -s -n 1 -p "Press any key to continue (ESC to terminate)..." key && [[ $key != '' ]] && [[ $key == $'\e' ]] && echo && exit
-echo
-docker compose logs --tail 10 --follow
+echo && docker compose logs --tail 10 --follow
